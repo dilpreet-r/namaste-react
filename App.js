d5ecc8e35a259c70dc427570fce2de4f26414fb4
Dilpreet Singh Randhawa <dilpreetr18@gmail.com>
2024-06-27 17:57:14 +0530

    added gitignore

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const heading = React.createElement("h1", {}, "Hello Dilpreet Randhawa");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement(
